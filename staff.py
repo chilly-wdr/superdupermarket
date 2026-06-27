7e65699fc8f6100595a82c2fad6d9a8cade07177
chillyw <g.dhanishka@gmail.com>
2026-06-28 00:24:47 +0400

    fixed the parameterized queries

--- staff.py
+++ staff.py
@@ -8,18 +8,24 @@ def add_staff():
         staff_id=int(input("enter new staff id: "))
         staff_name=input("enter staff name: ")
         mobile_no=int(input("enter staff mobile number: "))
-        cursor.execute("INSERT INTO STAFF VALUES({},'{}',{})".format(staff_id,staff_name,mobile_no))
+        cursor.execute("INSERT INTO STAFF (staff_id, staff_name, mobile_number) VALUES (%s, %s, %s)",
+    (staff_id, staff_name, mobile_no))
+    db.commit()
     
 def update_staff_name():
     staff_id=int(input("enter staff id: "))
     new_name=input("enter new staff name: ")
-    cursor.execute("UPDATE STAFF SET STAFF_NAME='{}' WHERE STAFF_ID={}".format(new_name,staff_id))
+    cursor.execute("UPDATE STAFF SET STAFF_NAME=%s WHERE STAFF_ID=%s",(new_name,staff_id))
+    db.commit()
 
 def update_staff_mobile():
     staff_id=int(input("enter staff id: "))
     new_mobile=int(input("enter new mobile number: "))
-    cursor.execute("UPDATE STAFF SET MOBILE_NUMBER={} WHERE STAFF_ID={}".format(new_mobile,staff_id))
+    cursor.execute("UPDATE STAFF SET MOBILE_NUMBER=%s WHERE STAFF_ID=%s",(new_mobile,staff_id))
+    db.commit()
 
 def delete_staff():
     staff_id=int(input("enter staff id: "))
     cursor.execute("DELETE FROM STAFF WHERE STAFF_ID={}".format(staff_id))
+    db.commit()
+
