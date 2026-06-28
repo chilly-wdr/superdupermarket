d3084c4655b45199a339bd391f8476092a933267
chillyw <g.dhanishka@gmail.com>
2026-06-28 18:23:12 +0400

    bugs

--- staff.py
+++ staff.py
@@ -8,22 +8,26 @@ def add_staff():
         mobile_no=int(input("enter staff mobile number: "))
         cursor.execute("INSERT INTO STAFF (staff_id, staff_name, mobile_number) VALUES (%s, %s, %s)",
     (staff_id, staff_name, mobile_no))
+    print("staff added successfully!")
     db.commit()
 
 def update_staff_name():
     staff_id=int(input("enter staff id: "))
     new_name=input("enter new staff name: ")
     cursor.execute("UPDATE STAFF SET STAFF_NAME=%s WHERE STAFF_ID=%s",(new_name,staff_id))
+    print("staff name updated successfully!")
     db.commit()
 
 def update_staff_mobile():
     staff_id=int(input("enter staff id: "))
     new_mobile=int(input("enter new mobile number: "))
     cursor.execute("UPDATE STAFF SET MOBILE_NUMBER=%s WHERE STAFF_ID=%s",(new_mobile,staff_id))
+    print("staff mobile number updated successfully!")
     db.commit()
 
 def delete_staff():
     staff_id=int(input("enter staff id: "))
     cursor.execute("DELETE FROM STAFF WHERE STAFF_ID=%s",(staff_id))
+    print("staff deleted successfully!")
     db.commit()
 
